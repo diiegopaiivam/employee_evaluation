fbbb80b87cfcca0e106f376667e821da9bc00a64
Diego Paiva <diiegopaiivam@gmail.com>
2021-11-27 15:12:44 -0300

    grid de colaboradores

--- app/controllers/admins_backoffice/employees_controller.rb
+++ app/controllers/admins_backoffice/employees_controller.rb
@@ -1,8 +1,54 @@
 class AdminsBackoffice::EmployeesController < AdminsBackofficeController
+    before_action :set_employee, only: [:show, :edit, :update, :destroy]
+
     def index
+        @employees = Employee.all
     end
 
     def new
         @employee = Employee.new
     end 
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @employee.update(employee_params)
+          redirect_to admins_backoffice_employees_path, notice: "Colaborador atualizado com sucesso"
+        else 
+          render :edit
+        end
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+        if @employee.save
+          redirect_to admins_backoffice_employees_path, notice: "Colaborador Salvo com sucesso!"
+        else 
+          render :new
+        end
+    end 
+
+    def destroy
+        if @employee.destroy
+          redirect_to admins_backoffice_employees_path, notice: "Colaborador excluído com sucesso"
+        else 
+          render :index 
+        end
+    end
+
+
+
+    private 
+
+        def set_employee
+            @employee = Employee.find(params[:id])
+        end 
+
+        def employee_params
+            params.require(:employee).permit(:name, :office)
+        end 
 end
